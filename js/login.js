99e1ea5e271b5e8cf484a911edf4f427975d4531
coder-binary1 <noman.g341@gmail.com>
2024-12-03 19:55:14 +0600

    add money, cach out & add money form added

--- js/login.js
+++ js/login.js
@@ -3,14 +3,41 @@
 //search: form submit reloading the page
 
 // step:1 -set event handler
+// document
+//   .getElementById("login-btn")
+//   .addEventListener("click", function (event) {
+//     // step:2 - prevent default behavior
+//     event.preventDefault();
+
+//     // step:3 - get the phone number
+//     const phoneNumber = document.getElementById("phone-number").value;
+//     const pinNumber = document.getElementById("pin-number").value;
+//     // console.log(phoneNumber, pinNumber);
+
+//     //step:4 -- validate phone and pin
+//     //this is temporary. We should not do like this
+//     if (phoneNumber === "01715") {
+//       console.log("you are logged in");
+//       //   step: 5 -- allow user to use the website
+//     } else {
+//       alert("Wrong phone or pin");
+//     }
+//   });
+
 document
   .getElementById("login-btn")
   .addEventListener("click", function (event) {
-    // step:2 - prevent default behavior
     event.preventDefault();
-    console.log("login button clicked");
 
-    // step:3 - get the phone number
-    const phoneNumber = document.getElementById("input-number");
-    console.log(phoneNumber.value);
+    //get phone & pin number
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    // console.log(phoneNumber, pinNumber);
+
+    //bad way to validate
+    if (phoneNumber === "01789343432" && pinNumber === "1234") {
+      window.location.href = "/home.html";
+    } else {
+      alert("phone or pin wrong");
+    }
   });
